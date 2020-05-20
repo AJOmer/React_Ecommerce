73869e74e1ad2ff9b54c99a2bbec1a566bf6edbe
Ahmed <ahmedjomer@gmail.com>
2020-05-19 17:03:13 -0700

    auth component finished

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,92 @@
-import React    from "react";
-import template from "./Login.jsx";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-class Login extends React.Component {
-  render() {
-    return template.call(this);
-  }
-}
+// CSS
+import "./Login.css";
 
-export default Login;
+const Login = ({ login, isAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    // it changes the value of the target every keystroke
+    const onChange = (e) =>
+        setFormData({...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        login(email, password);
+    };
+
+    // Redirects to /account after a successful login
+    if (isAuthenticated) {
+        return <Redirect to = "/account" / > ;
+    }
+
+    return ( <
+        div className = "wrapper-login" >
+        <
+        h1 > Login < /h1> <
+        form onSubmit = {
+            (e) => onSubmit(e) } >
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "email" > Email < /label> <
+        input className = "form-control"
+        type = "email"
+        name = "email"
+        value = { email }
+        onChange = {
+            (e) => onChange(e) }
+        required /
+        >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        label htmlFor = "password" > Password < /label> <
+        input className = "form-control"
+        type = "password"
+        name = "password"
+        value = { password }
+        onChange = {
+            (e) => onChange(e) }
+        minLength = "6"
+        required /
+        >
+        <
+        /div> <
+        div className = "login-button" >
+        <
+        input type = "submit"
+        className = "btn btn-dark btn-block"
+        value = "Login" /
+        >
+        <
+        /div> <
+        /form> <
+        p >
+        Don 't have an account?{" "} <
+        Link to = "/account/register" > Create an account < /Link> <
+        /p> <
+        /div>
+    );
+};
+
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
