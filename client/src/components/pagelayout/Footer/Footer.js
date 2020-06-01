35a8db730ddfdb00ef5eff3095a990abda22f097
Ahmed <ahmedjomer@gmail.com>
2020-06-01 16:51:15 -0700

    footer details

---
+++ client/src/components/pagelayout/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return ( <
+        div className = "footer-wrapper" >
+        <
+        div className = "footer-container" >
+        <
+        div className = "footer-connect" >
+        <
+        p className = "connect-subheader" > Connect With Us < /p> <
+        div className = "newsletter-form input-group" >
+        <
+        input type = "text"
+        placeholder = "Email Address" > < /input> <
+        button > subscribe < /button> <
+        /div> <
+        /div> <
+        div className = "footer-ourcompany" >
+        <
+        p className = "connect-subheader" > Company < /p> <
+        ul >
+        <
+        li > About TeeTime < /li> <
+        li > < /li> <
+        /ul> <
+        /div> <
+        /div> <
+        /div>
+    );
+};
