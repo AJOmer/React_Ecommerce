4303ef6750449dd4b4881a17fecbea52e0710f63
Ahmed Jamal Omer <52680408+AJOmer@users.noreply.github.com>
2020-07-10 17:31:38 -0700

    Add files via upload

---
+++ client/src/shirtcollection/collection/Shirt.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./ShirtCollection.css";
+
+const Shirt = ({ shirt: { _id, name, brand, images, retail_price } }) => {
+    return ( <
+        div className = "grid-latestrelease-column col-3" >
+        <
+        Link to = { `/products/shirts/${_id}` } >
+        <
+        div className = "shirt_image" >
+        <
+        img alt = "My Shirts"
+        src = { images[0] }
+        />{" "} <
+        /div>{" "} <
+        div className = "shirt_specs" >
+        <
+        p className = "shirt_specs_brand" > { brand } < /p>{" "} <
+        p className = "shirt_specs_name" > { name } < /p>{" "} <
+        p className = "shirt_specs_price" > $ { retail_price } < /p>{" "} <
+        /div>{" "} <
+        /Link>{" "} <
+        /div>
+    );
+};
+
+Shirt.propTypes = {
+    shirt: PropTypes.object.isRequired,
+};
+
+export default Shirt;
